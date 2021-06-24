b4b311150578e2317bb065aae4918104db17fe09
Wenfang Du <duwenfangcn@163.com>
2021-06-24 09:37:07 +0800

    added repro

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const cascader = ref([])
+    let id = 0
+
+    return () => (
+      <ElCascader
+        vModel={cascader.value}
+        props={{
+          lazy: true,
+          // 👇 triggers on every re-render, should be checked by content, e.g. using lodash's isEqual
+          lazyLoad(node, resolve) {
+            const { level } = node
+            setTimeout(() => {
+              const nodes = Array.from({ length: level + 1 }).map(() => ({
+                value: ++id,
+                label: `选项${id}`,
+                leaf: level >= 2,
+              }))
+              // 通过调用resolve将子节点数据返回，通知组件数据加载完成
+              resolve(nodes)
+            }, 1000)
+          },
+        }}
+      />
+    )
+  },
+})
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,8 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
-</template>
-
-<script>
-  import HelloWorld from './components/HelloWorld.vue'
-
-  export default {
-    name: 'App',
-    components: {
-      HelloWorld,
-    },
-  }
-</script>
+<script src="./App.jsx"></script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  body {
+    background: #000;
+    color: #eee;
   }
 </style>
